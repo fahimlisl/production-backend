7d767c64af72269d059ac66bdc7896b8bf480972
fahimlisl <developerfahim134@gmail.com>
2025-11-02 22:45:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+// const port = 4000
+
+app.get('/',(req,res) => {
+    res.send('hello world!')
+})
+
+app.listen(process.env.PORT,() => {
+    console.log(`example app is listening on port ${process.env.PORT}`);
+    
+})
